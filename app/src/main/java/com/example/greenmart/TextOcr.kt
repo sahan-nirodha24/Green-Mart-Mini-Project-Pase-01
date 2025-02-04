d9dae0310152e20ec11a8845127f4825252114ab
sahan-nirodha24 <nsahan2001@gmail.com>
2025-02-04 22:39:51 +0530

    First Commit

---
+++ app/src/main/java/com/example/greenmart/TextOcr.kt
@@ -0,0 +1,14 @@
+package com.example.greenmart
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class TextOcr : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_text_ocr)
+    }
+}
